5c175ee51b73961d2360e67b52f4d0bf92a3f82d
unknown <riley.schuit@gmail.com>
2018-03-02 14:04:15 -0800

    correcting many errors around Step 32

--- src/App.js
+++ src/App.js
@@ -2,25 +2,35 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-import SearchBar from '..Components/SearchBar/SearchBar';
-import SearchBarResults from '..Components/SearchResults/SearchResults';
-import Playlist from '..Components/Playlist/Playlist';
+import SearchBar from './Components/SearchBar/SearchBar';
+import SearchBarResults from './Components/SearchResults/SearchResults';
+import Playlist from './Components/Playlist/Playlist';
 
 class App extends React.Component {
   constructor(props) {
+    super()
+
+    this.state = {
+      searchResults: [
+        {name: "a", artist: "b", album: "c"},
+        {name: "d", artist: "e", album: "f"},
+      ]
+    }
   }
 
   render() {
-    <div>
-      <h1>Ja<span className="highlight">mmm</span>ing</h1>
-      <div className="App">
-        <!-- Add a SearchBar component -->
-        <div className="App-playlist">
-          <!-- Add a SearchResults component -->
-          <!-- Add a Playlist component -->
+    return (
+      <div>
+        <h1>Ja<span className="highlight">mmm</span>ing</h1>
+        <div className="App">
+          {/*  Add a SearchBar component */}
+          <div className="App-playlist">
+            <SearchBarResults searchResults={this.state.searchResults} onAdd={this.addTrack} />
+            {/* Add a Playlist component */}
+          </div>
         </div>
       </div>
-    </div>
+    )
   }
 }
 
