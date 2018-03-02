fee495e0a1a1b73615db717b5d6aa0b4413e3a84
unknown <riley.schuit@gmail.com>
2018-03-02 11:28:57 -0800

    first commit up to step 13

---
+++ src/App.js
@@ -0,0 +1,27 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+import SearchBar from '..Components/SearchBar/SearchBar';
+import SearchBarResults from '..Components/SearchResults/SearchResults';
+import Playlist from '..Components/Playlist/Playlist';
+
+class App extends React.Component {
+  constructor(props) {
+  }
+
+  render() {
+    <div>
+      <h1>Ja<span className="highlight">mmm</span>ing</h1>
+      <div className="App">
+        <!-- Add a SearchBar component -->
+        <div className="App-playlist">
+          <!-- Add a SearchResults component -->
+          <!-- Add a Playlist component -->
+        </div>
+      </div>
+    </div>
+  }
+}
+
+export default App;
