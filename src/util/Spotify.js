89e565da38f2d1b0bd46b5460a7e2d0a5edc9a35
Riley Schuit <riley.schuit@gmail.com>
2018-03-05 17:08:37 -0800

    Spotify draft ideas

---
+++ src/util/Spotify.js
@@ -0,0 +1,27 @@
+// DRAFT
+export default class Spotify {
+  // Authentication and Authorization
+  getauthtoken() { }
+
+  setauthorizationheader() { }
+
+  setuserid() { }
+
+  // Search
+  searchtracks(searchterm) { }
+
+  // Collect playlists
+  loadplaylists(playlistId) { }
+
+  // Create Playlist
+  createplaylist() { }
+
+  // Delete Playlist
+  removeplaylist(playlistId) { }
+
+  // Add track
+  addtrack(trackid,playlistId) { }
+
+  // Remove track
+  removetrack(trackid,playlistId) { }
+}
